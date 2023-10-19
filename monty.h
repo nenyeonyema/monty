2a0318d8e89772ca98acadd0dacdeba2b8274d94
nenyeonyema <chinenyeonyema1@gmail.com>
2023-10-19 14:07:03 -0700

    adds a new node at the beginning and end, plus free memory

--- monty.h
+++ monty.h
@@ -20,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,10 +35,12 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void shared_top(stack_t **head, unsigned int count_line);
+stack_t *add_new_end_node(stack_t **head, const int num);
+stack_t *add_new_beg_node(stack_t **head, const int num);
+void _freedlist(stack_t *head);
 
 #endif
